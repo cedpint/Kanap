2d96a3fe1aadaaf0cd37ca67df8cbef7f1308d97
cedric <pintado.cedric@gmail.com>
2022-09-26 15:10:24 +0200

    test

--- front/js/product.js
+++ front/js/product.js
@@ -46,10 +46,18 @@ Dans la réponse (return) de notre précédent .then nous allons demander à ré
           <p class="productDescription">${product.description}</p>
         </article>
       </a>
+
+      <a href="./product.html?id=${product._id}">
+        <article>
+          <img src="${product.imageUrl}" alt=${product.altTxt}">
+          <h3 class="productName">${product.name}</h3>
+          <p class="productDescription">${product.description}</p>
+        </article>
+      </a>
     
     `;
     }
   });
 
 
-  /*TESTTTTTTTTT */
+  /*TESTTTTtttttttttttttTTTTT */
